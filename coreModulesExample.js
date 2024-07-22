c088bd4c0493b8b13f5ba7a5e5aaaba026d16c69
Lisa <naoliyisak@gmail.com>
2024-07-22 15:34:03 -0700

    Path core module exmaple

---
+++ coreModulesExample.js
@@ -0,0 +1,8 @@
+// // // Path core module exmaple
+const path = require("path");
+var pathObj = path.parse(__filename);
+
+// console.log(__filename); // will take us to file name
+// console.log(__dirname); // will take us to directer name
+
+// console.log(pathObj);
