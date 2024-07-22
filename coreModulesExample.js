1d463eb160d75f421e91ffb8a66078059df290b7
Lisa <naoliyisak@gmail.com>
2024-07-22 15:50:23 -0700

    os core module example

--- coreModulesExample.js
+++ coreModulesExample.js
@@ -6,3 +6,15 @@ var pathObj = path.parse(__filename);
 // console.log(__dirname); // will take us to directer name
 
 // console.log(pathObj);
+
+
+// // // os core module example
+const os = require("os");
+// console.log(os)
+var totalMemo = os.totalmem();
+var memo = os.freemem();
+
+console.log("total Memory", totalMemo);
+console.log("free Memory", memo);
+console.log(os.platform());
+console.log(os.arch());
