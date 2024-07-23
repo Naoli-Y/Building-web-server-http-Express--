1088c90fb4f1fbe08fa82aa8526323081d423edb
Lisa <naoliyisak@gmail.com>
2024-07-22 18:07:23 -0700

    fileSystem core module example

--- coreModulesExample.js
+++ coreModulesExample.js
@@ -14,7 +14,34 @@ const os = require("os");
 var totalMemo = os.totalmem();
 var memo = os.freemem();
 
-console.log("total Memory", totalMemo);
-console.log("free Memory", memo);
-console.log(os.platform());
-console.log(os.arch());
+// console.log("total Memory", totalMemo);
+// console.log("free Memory", memo);
+// console.log(os.platform());
+// console.log(os.arch());
+
+
+// // // fileSystem core module example
+const { readdir, mkdir, readFile, writeFile } = require("fs");
+
+// readdir("./", (err, Files) => {
+//     if (err) console.log("error", err);
+//     else console.log(Files);
+// });
+
+  // to creat folder
+// mkdir("test", (err, path) => {
+//     if (err) console.log("error", err);
+//     console.log(path); // if recursive is false it returns undefined
+// })
+
+
+//  readFile("example.txt", "utf8", (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+//  });
+
+
+ writeFile("message.txt", "Hello class", (err) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+ });
