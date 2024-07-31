58c2dabd5fa8610ea32a5d686ba07ad63ce81b28
Lisa <naoliyisak@gmail.com>
2024-07-30 23:59:42 -0700

    Serving static HTML file with HTTP

--- index.js
+++ index.js
@@ -19,25 +19,54 @@ const http = require("http");
 
 
 /// // // server create with different path
-const server = http.createServer((req, res) => {
-    console.log(req.url);
-    if (req.url === "/") {
-        res.write(
-            "<h1>This is home page.</1>");
-        res.end();
-    } else if (req.url === "/about") {
-        res.write("<h1>This is about page.</h1>");
-        res.end();
-    } else {
-        res.write("<h1>Page Not Found!</h1>");
-        res.end();
-    }
-});
+// const server = http.createServer((req, res) => {
+//     console.log(req.url);
+//     if (req.url === "/") {
+//         res.write(
+//             "<h1>This is home page.</1>");
+//         res.end();
+//     } else if (req.url === "/about") {
+//         res.write("<h1>This is about page.</h1>");
+//         res.end();
+//     } else {
+//         res.write("<h1>Page Not Found!</h1>");
+//         res.end();
+//     }
+// });
 
 // PORT
-const PORT = 5001;
+// const PORT = 5001;
 
 // server listen port
-server.listen(PORT, () => {
-    console.log(`Server is running on PORT: http://localhost:${PORT}`);
+// server.listen(PORT, () => {
+//     console.log(`Server is running on PORT: http://localhost:${PORT}`);
+// });
+
+
+
+/// /// /// ******** Serving static HTML file with HTTP ****** /// /// ///
+
+const http = require("http");
+const fs = require("fs");
+// const url = require("url");
+
+const serverMyStaticSite = http.createServer((req, res) => {
+   
+    let reqPath = req.url;
+    console.log("requested path >>> ", reqPath);
+
+    if (reqPath === "/") {
+        reqPath = "/index.html"
+    }
+
+    let requestedFile = "./bob" + reqPath;
+    console.log("requested file >>> ", requestedFile);
+
+    fs.readFilr(requestedFile, (err, content)) => {
+        
+    }
+})
+
+serverMyStaticSite.listen(5050, () => {
+    console.log("Sevrver is running on PORT: http://localhost:5050");
 });
